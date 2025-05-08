fe78b06d295d28cd6811b0d345b4580c09a1e77e
Elvin-Mworia <elvinmworia@gmail.com>
2025-05-09 01:59:16 +0300

    Refactor CardHomepage component layout and styles for improved visual consistency

--- pages/components/CardHomepage.js
+++ pages/components/CardHomepage.js
@@ -3,12 +3,12 @@ import Image from 'next/image'
 export default function CardHomepage({name,testimonial,imagelink }) {
     return(
         <>
-        <Card minH={350} minW={250} maxW={250} borderRadius="5px" boxShadow='lg' bg='wheat' style={{display:"flex",justifyContent:"center",alignItems:"center"}} >
+        <Card minH={350} minW={200} maxW={200} borderRadius="5px" boxShadow='lg' bg='wheat' style={{display:"flex",justifyContent:"center",alignItems:"center",flexShrink:0}} >
   <CardBody>
   
     <Stack  spacing='1' >
-        <Heading size='sm'>{name}</Heading>
-        <div style={{height:"150px",width:"150px",marginTop:".1em",marginBottom:".05em",display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center",marginLeft:"10%",marginRight:"10%"}} >   <Image
+        <Heading size='sm' style={{marginRight:"20%"}}>{name}</Heading>
+        <div style={{height:"120px",width:"150px",marginTop:".1em",marginBottom:".05em",display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center",marginLeft:"5%"}} >   <Image
       src={imagelink}
       alt="testimonial"
      style={{borderRadius:"5px",height:"100%",width:"100%",objectFit:"cover",backgroundOrigin:"center",backgroundPosition:"center"}}
@@ -18,7 +18,7 @@ export default function CardHomepage({name,testimonial,imagelink }) {
   </CardBody>
   <CardFooter>
     
-  <Text color="black" fontSize='sm' fontWeight={700} textAlign="center" style={{marginLeft:"10%"}}>
+  <Text color="green" fontSize='sm' fontWeight={300} textAlign="center" style={{marginLeft:"10%"}}>
         {testimonial}
       </Text>
      
