c8f92461a0d1d144fba57cddabbf69482f36875a
Elvin-Mworia <elvinmworia@gmail.com>
2025-05-13 17:24:02 +0300

    Fix import path for logo styles and ensure proper return statement in logo component

--- pages/components/logo.js
+++ pages/components/logo.js
@@ -1,8 +1,18 @@
 import Image from "next/image";
 import svg from "../../public/image2.svg";
-import styles from "../../styles/logo.module.scss";
+import styles from "../../styles/Home.module.scss";
 function logo(){
 
-    return()
+    return(
+        <div className={styles.logo}>
+            <Image 
+                src={svg}
+                alt="Hastle Logo"
+                width={150}
+                height={50}
+                priority
+            />
+        </div>
+    )
 }
 export default logo;
