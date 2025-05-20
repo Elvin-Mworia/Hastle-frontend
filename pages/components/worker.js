4a0064a76d260602addcad386adf85a3a942810f
Elvin-Mworia <elvinmworia@gmail.com>
2025-05-20 17:29:14 +0300

    Add Worker component with form handling and fade-in animations for improved user experience

---
+++ pages/components/worker.js
@@ -0,0 +1,108 @@
+import {useState} from "react";
+import {motion} from 'framer-motion';
+import {useSelector} from "react-redux"
+import Image from "next/image";
+import {useFormik} from 'formik';
+import styles from '../../styles/client.module.scss';
+import { basicSchema } from "../../utils/basicSchema";
+
+const fadeInAnimationForm={
+  initial: { opacity: 0, x: 20 },
+  animate: { opacity: 1, x: 0, transition: { delay: .8, duration: 0.3, ease: "easeIn" } },
+};
+const fadeInAnimationH2={
+  initial: { opacity: 0, y: 20 },
+  animate: { opacity: 1, y: 0, transition: { delay: 1, duration: 0.5, ease: "easeIn" } },
+};
+
+export default function Worker(){
+  const{client,worker}=useSelector((state)=>(state.user));
+  const [firstname,setFirstName]=useState("");
+  const [lastname,setLastName]=useState("");
+  const [email,setEmail]=useState("");
+  const [phone,setPhone]=useState("");
+  const [password,setPassword]=useState("");
+  const [confirmPassword,setconfirmPassword]=useState("");
+
+const  Submit=(e)=>{
+  const fullname=firstname+" "+lastname;
+  console.log(fullname);
+
+}
+    return(<motion.div variants={fadeInAnimationForm}
+            initial="initial"
+            animate="animate"
+            className={styles.container}>
+        
+        <div className={styles.clientContainer}>
+        <motion.h2  s
+        variants={fadeInAnimationH2}
+        initial="initial"
+        animate="animate">Expert Sign Up</motion.h2>
+        <form className={styles.inputWrapper} onSubmit={Submit}>
+            <div className={styles.names}>
+                <div className={styles.soloname}>
+                < label for="firstName">First Name</label>
+                <input type="text"
+                value={firstname}
+                onChange={(e)=>{setFirstName(e.target.value)}}
+                
+                name="firstName"
+
+                />
+                </div>
+                <div className={styles.soloname}>
+                <label for="secondName">Second Name</label>
+                <input type='text' value={lastname}
+                onChange={(e)=>{setLastName(e.target.value)}}
+              
+                name="secondName"
+                />
+                </div>
+                
+            </div>
+          
+            <label for="email">Email</label>
+                <input type="email"
+                value={email}
+              onChange={(e)=>{setEmail(e.target.value)}}
+              
+                name="email"
+                />
+         
+            <label for="phone">Phone Number</label>
+                <input type="telephone"
+                  value={phone}
+                  onChange={(e)=>{setPhone(e.target.value)}}
+                
+                name="phone"
+                />
+          
+ 
+            <label for="password">Password</label>
+                <input type="password"
+                  value={password}
+                onChange={(e)=>{setPassword(e.target.value)}}
+               
+                name="password"
+
+                />
+         
+       
+            <label for="confirmpassword">Confirm Password</label>
+                <input type="password"
+                  value={confirmPassword}
+                 onChange={(e)=>{setconfirmPassword(e.target.value)}}
+                  name="confirmpassword"
+              />
+                {/* {errors.confirmPassword && touched.confirmPassword &&
+                (<p className={styles.errors}>{errors.confirmPassword}</p>)} */}
+      <button type="submit"  onSubmit={Submit}>Submit</button>
+            </form>
+ 
+ 
+      </div>
+      </motion.div>
+        
+    )
+}
